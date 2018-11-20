002869ec3adf00b6d03a260341d21d36ac0fa5ed
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-11-20 14:56:48 +0100

    Sounds will now also be made when task progress messages are received

--- ropod_sound_communication/ros/src/ropod_sound_communication/sound_communication.py
+++ ropod_sound_communication/ros/src/ropod_sound_communication/sound_communication.py
@@ -3,35 +3,72 @@ import wave
 import pyaudio
 import yaml
 import rospy
-from ropod_ros_msgs.msg import StateInfo
+from ropod_ros_msgs.msg import StateInfo, Status
+from ropod_ros_msgs.msg import TaskProgressGOTO, TaskProgressDOCK
 
 class SoundCommunicator(object):
     def __init__(self):
         self.sound_config = rospy.get_param('~sound_config', '')
         self.sound_collection = rospy.get_param('~sound_collection', 'willow-sound')
         self.sound_request_topic = rospy.get_param('~sound_request_topic', '/state_info')
+        self.go_to_progress_topic = rospy.get_param('~go_to_progress_topic', '/task_progress/goto')
+        self.dock_progress_topic = rospy.get_param('~dock_progress_topic', '/task_progress/dock')
 
         self.sound_file_dictionary = dict()
         with open(self.sound_config, 'r') as sound_config_file:
             self.sound_file_dictionary = yaml.load(sound_config_file)
 
         self.audio_manager = pyaudio.PyAudio()
-        rospy.Subscriber(self.sound_request_topic, StateInfo, self.sound_cb)
+        self.state_info_sub = rospy.Subscriber(self.sound_request_topic,
+                                               StateInfo,
+                                               self.sound_cb)
+        self.go_to_progress_sub = rospy.Subscriber(self.go_to_progress_topic,
+                                                   TaskProgressGOTO,
+                                                   self.go_to_progress_cb)
+        self.dock_progress_sub = rospy.Subscriber(self.dock_progress_topic,
+                                                  TaskProgressDOCK,
+                                                  self.dock_progress_cb)
 
     def sound_cb(self, msg):
         sound = msg.state
         try:
             sound_file = self.sound_file_dictionary[sound]
-        except:
-            rospy.logerr('Sound %s does not exists', sound)
+            self.__play_sound(sound_file)
+        except Exception as exc:
+            rospy.logerr('[sound_communication/sound_cb] Sound %s does not exists', sound)
+            rospy.logerr(str(exc))
             return
 
-        sound_file_path = join(self.sound_collection, sound_file)
-        self.__play_sound(sound_file_path)
+    def go_to_progress_cb(self, msg):
+        try:
+            if msg.status.status_code == Status.SUCCESS:
+                self.__play_sound(self.sound_file_dictionary['success'])
+            elif msg.status.status_code == Status.FAILED:
+                self.__play_sound(self.sound_file_dictionary['error'])
+            else:
+                rospy.loginfo('[sound_communication/go_to_progress_cb] No sound for status %d',
+                              msg.status.status_code)
+        except Exception as exc:
+            rospy.logerr('[sound_communication/go_to_progress_cb] %s', str(exc))
+            return
+
+    def dock_progress_cb(self, msg):
+        try:
+            if msg.status.status_code == Status.SUCCESS:
+                self.__play_sound(self.sound_file_dictionary['success'])
+            elif msg.status.status_code == Status.FAILED:
+                self.__play_sound(self.sound_file_dictionary['error'])
+            else:
+                rospy.loginfo('[sound_communication/dock_progress_cb] No sound for status %d',
+                              msg.status.status_code)
+        except Exception as exc:
+            rospy.logerr('[sound_communication/dock_progress_cb] %s', str(exc))
+            return
 
     def __play_sound(self, sound_file):
         try:
-            wavefile = wave.open(sound_file, 'rb')
+            sound_file_path = join(self.sound_collection, sound_file)
+            wavefile = wave.open(sound_file_path, 'rb')
             data = wavefile.readframes(1024)
             data_format = self.audio_manager.get_format_from_width(wavefile.getsampwidth())
             stream = self.audio_manager.open(format=data_format,
